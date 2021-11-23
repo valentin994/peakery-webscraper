c05c3af4ece2efd48a0a1c16ebc78146e420bed3
valentin994 <valentin.vareskic@outlook.com>
2021-11-23 18:06:05 +0100

    fetch first page elevation in meters

--- main.py
+++ main.py
@@ -5,10 +5,15 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 
-if __name__ == "main":
-    driver = webdriver.Firefox()
+if __name__ == "__main__":
+    driver = webdriver.Firefox(executable_path="/Users/vareskic/Downloads/geckodriver")
     driver.implicitly_wait(30)
     driver.get("https://peakery.com/peaks/")
-    WebDriverWait(driver, 60).until(EC.visibility_of_all_elements_located((By.ID, "peaks-table-body")))
-    text = driver.find_element(By.ID,"peaks-table-body")
-    print(text.get_attribute("innerHTML"))
+    WebDriverWait(driver, 60).until(
+        EC.visibility_of_all_elements_located((By.ID, "peaks-table-body"))
+    )
+    text = driver.find_element(By.ID, "peaks-table-body")
+    soup = BeautifulSoup(text.get_attribute("innerHTML"), "html.parser")
+    table_rows = soup.findAll("td")
+    peak = soup.findAll("span", attrs={"class": "peak-elevation-meters"})
+    [print(x.text) for x in peak]
